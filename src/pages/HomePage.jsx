58afa5f0114f07b6163b66d1a8528e0a444e4b3e
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-09-28 15:42:17 -0300

    Adicionei a HomePage com o styled component para versão Desktop. Adicionei o GlobalStyled também

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,50 +6,53 @@ import goalStore from "../assets/goalStore.png"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 
+import { HomeContainer, ProfileInfoButton, ProfileInfoContainer, ProfileInfoDescription, ProfileInfoTitle, ProfileSectionContainer,
+         ProfileSectionIMG, ProjectCardContainer, ProjectCardDescription, ProjectCardIMG, ProjectCardName, ProjectCardWrapper,
+         ProjectLink, ProjectSectionContainer, ProjectSectionTitle } from "../styles/HomeStyles"
+
 export default function HomePage() {
   return (
     <>
       <Header />
-      <main>
-        <section id="sobre">
-          <img src={igor} alt="Igor Perdigão" />
-          <div>
-            <h2>Sobre mim</h2>
-            <p>Eu sou um Programador Front End, tenho experiência e conhecimento criando aplicações com HTML, CSS, JavaScript, React e Node.</p>
-            <button>Entre em contato</button>
-          </div>
-        </section>
+      <HomeContainer>
+        <ProfileSectionContainer id="sobre">
+          <ProfileSectionIMG src={igor} alt="Igor Perdigão" />
+          <ProfileInfoContainer>
+            <ProfileInfoTitle>Sobre mim</ProfileInfoTitle>
+            <ProfileInfoDescription>Eu sou um Programador Front End, tenho experiência e conhecimento criando aplicações com HTML, CSS, JavaScript, React e Node.</ProfileInfoDescription>
+            <ProfileInfoButton>Entre em contato</ProfileInfoButton>
+          </ProfileInfoContainer>
+        </ProfileSectionContainer>
 
-        <section className="projetos" id="projetos-id">
-          <h2>Principais Projetos</h2>
+        <ProjectSectionContainer id="projetos-id">
+          <ProjectSectionTitle>Principais Projetos</ProjectSectionTitle>
+          <ProjectCardContainer>
+            <ProjectCardWrapper>
+              <ProjectCardIMG src={goalStore} alt="Projeto Goal Store Shirts" />
+              <ProjectCardName>Goal Store Shirts</ProjectCardName>
+              <ProjectCardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad error harum doloribus labore,
+                odio necessitatibus officia illum rem veritatis possimus aut</ProjectCardDescription>
+              <ProjectLink href="https://starbucks-rodolfo.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Projeto</ProjectLink>
+            </ProjectCardWrapper>
 
-          <div className="card-projetos">
-            <div className="card">
-              <img src={goalStore} alt="Projeto Goal Store Shirts" />
-              <h5>Starbucks</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad error harum doloribus labore,
-                odio necessitatibus officia illum rem veritatis possimus aut</p>
-              <a href="https://starbucks-rodolfo.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-            </div>
+            <ProjectCardWrapper>
+              <ProjectCardIMG src={financialSystem} alt="Projeto de Controle Financeiro" />
+              <ProjectCardName>Financial System</ProjectCardName>
+              <ProjectCardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad error harum doloribus labore,
+                odio necessitatibus officia illum rem veritatis possimus aut</ProjectCardDescription>
+              <ProjectLink href="https://cronometro-mpdz.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Projeto</ProjectLink>
+            </ProjectCardWrapper>
 
-            <div className="card">
-              <img src={financialSystem} alt="Projeto de Controle Financeiro" />
-              <h5>Cronometro</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad error harum doloribus labore,
-                odio necessitatibus officia illum rem veritatis possimus aut</p>
-              <a href="https://cronometro-mpdz.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-            </div>
-
-            <div className="card">
-              <img src={movie} alt="MovieDB icon" />
-              <h5>Movie Streaming</h5>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad error harum doloribus labore,
-                odio necessitatibus officia illum rem veritatis possimus aut</p>
-              <a href="https://peaceful-starlight-02739b.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-            </div>
-          </div>
-        </section>
-      </main>
+            <ProjectCardWrapper>
+              <ProjectCardIMG src={movie} alt="MovieDB icon" />
+              <ProjectCardName>Movie Streaming</ProjectCardName>
+              <ProjectCardDescription>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ad error harum doloribus labore,
+                odio necessitatibus officia illum rem veritatis possimus aut</ProjectCardDescription>
+              <ProjectLink href="https://peaceful-starlight-02739b.netlify.app/" target="_blank" rel="noopener noreferrer">Ver Projeto</ProjectLink>
+            </ProjectCardWrapper>
+          </ProjectCardContainer>
+        </ProjectSectionContainer>
+      </HomeContainer>
       <Footer />
     </>
   )
---
+++ src/styles/HomeStyles.jsx
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main``
+
+//PROFILE SECTION
+export const ProfileSectionContainer = styled.section`
+    display: flex;
+    align-items: center;
+    padding: 60px 15%;
+    background-color: #000000;
+`
+export const ProfileSectionIMG = styled.img`
+    height: 400px;
+    border-radius: 100%;
+    /* border: 2px solid #f9004d; */
+    border: 2px solid white;
+    margin-right: 30px;
+`
+export const ProfileInfoContainer = styled.div`
+`
+export const ProfileInfoTitle = styled.h2`
+    color: white;
+    font-size: 75px;
+`
+export const ProfileInfoDescription = styled.p`
+    color: #e2b9c6;
+    font-size: 24px;
+    margin-top: 15px;
+    margin-bottom: 60px;
+`
+export const ProfileInfoButton = styled.button`
+    color: white;
+    background: #f9004d;
+    height: 50px;
+    width: 180px;
+    font-size: 18px;
+    border: none;
+    border-radius: 26px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: black;
+        border: 3px solid #f9004d;
+        transition: 0.5s;
+    }
+`
+
+//PROJECT SECTION
+export const ProjectSectionContainer = styled.section`
+    background-color: #191919;
+    padding-top: 50px;
+    padding-bottom: 100px;
+`
+export const ProjectSectionTitle = styled.h2`
+    color: white;
+    font-size: 75px;
+    text-align: center;
+`
+export const ProjectCardContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    margin-top: 100px;
+`
+export const ProjectCardWrapper = styled.div`
+    background-color: #000000;
+    height: 365px;
+    width: 335px;
+    border-radius: 20px;
+    padding: 20px 35px;
+    text-align: center;
+`
+export const ProjectCardIMG = styled.img`
+    height: 100px;
+`
+export const ProjectCardName = styled.h5`
+    color: white;
+    font-size: 24px;
+`
+export const ProjectCardDescription = styled.p`
+    color: #e2b9c6;
+    margin-top: 20px;
+    margin-bottom: 40px;
+`
+export const ProjectLink = styled.a`
+    background-color: #f9004d;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 25px;
+    cursor: pointer;
+    transition: 0.5s;
+    text-decoration: none;
+
+    &:hover {
+        background-color: transparent;
+        border: 2px solid #f9004d;
+    }
+`
