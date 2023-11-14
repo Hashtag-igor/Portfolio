36081d3d38496917e269449a330c7fc9a8497891
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-11-14 17:12:23 -0300

    Adiciona o arquivo ProjectsData que contém as informações de todos os cards em objetos que serão consumidos na HomePage, substituindo os cards que se repetiam por ali anteriormente. Adiciona o arquivo Card que recebe as props do map do HomePage e faz a estrutura do Card.

---
+++ src/components/ProjectsData.jsx
@@ -0,0 +1,39 @@
+// projectData.js
+
+import goalStore from "../assets/goalStore.png";
+import movie from "../assets/movie.png";
+import financialSystem from "../assets/financialSystem.png";
+import pokedex from "../assets/pokedex.png"
+
+const projectsData = [
+    {
+      img: goalStore,
+      alt: "Projeto Goal Store Shirts",
+      name: "Goal Store Shirts",
+      description: "Neste projeto fullstack, utilizei React, Node.js e MongoDB. O backend apresenta uma API personalizada para gerenciar camisas de time, consumida pelo frontend. O MongoDB armazena dados de usuários para registro e login. Usuários autenticados possuem acesso total, enquanto visitantes têm funcionalidades limitadas, como adicionar itens ao carrinho ou acessar áreas restritas.",
+      link: "/goalstore"
+    },
+    {
+      img: movie,
+      alt: "Projeto Movie Streaming",
+      name: "Movie Streaming",
+      description: "Este projeto desenvolvido em React é um serviço de streaming que proporciona aos usuários acesso a um vasto catálogo de filmes e programas de TV, incluindo os títulos mais bem avaliados, populares, próximos e atualmente em exibição. Com a capacidade de pesquisar títulos de todo o mundo, os usuários podem explorar as avaliações críticas, a sinopse e muitos outros detalhes relevantes para tomar decisões informadas sobre o que assistir.",
+      link: "/moviestreaming"
+    },
+    {
+      img: financialSystem,
+      alt: "Projeto Financial System",
+      name: "Financial System",
+      description: "O Projeto está em desenvolvimento.. Em breve estará disponivel.",
+      link: "/financialsystem"
+    },
+    {
+      img: pokedex,
+      alt: "Projeto Pokedex",
+      name: "Pokedex",
+      description: "Desenvolvi uma Pokédex interativa conectada à API de Pokémon, exibindo os 150 primeiros Pokémon em cards com imagem, nome e tipo para uma visão rápida. Ao clicar em um card, o usuário é redirecionado automaticamente para a página de perfil do Pokémon, apresentando informações detalhadas para uma experiência completa de descoberta e exploração pelos fãs de Pokémon.",
+      link: "/pokedex"
+    },
+  ];
+
+export default projectsData;
