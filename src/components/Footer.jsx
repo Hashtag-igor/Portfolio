3e5466653e16db04802a24b47fdbf1a452ed40c6
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-11-29 17:46:16 -0300

    Adiciona a estilização do component Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,62 +1,35 @@
-import React from 'react'
-
-import { FooterContainer, FooterName, FooterSocialContainer, FooterSocialLinkWrapper, GithubIcon, LinkedinIcon, FooterTitle,
-         FooterInfoContainer, FooterSocialWrapper, FooterLink, RightsReservedContainer, MailIcoon, FooterInfoWrapper,
-         PhoneIcon } from "../styles/FooterStyles"
+import { FooterContainer, FooterName,  GithubIcon, LinkedinIcon, RightsReservedContainer, WhatsappIcon, InstagramIcon, FooterLinksContainer, 
+         FooterProfileWrapper, FooterProfileContainer, FooterTitle, FooterDescription, FooterNumber, FooterReserved, FooterLinksWrapper} from "../styles/FooterStyles"
 
 export default function Footer() {
   return (
-    <div style={{background: "#fff" ,padding: "80px 0 20px 0"}} id='fale-comigo'>
-      <div style={{textAlign: "center", color: "#000000", width: "40%", margin: "auto", border: "2px solid red"}}>
-        <div>
-          <h1>Vamos conversar?</h1>
-          <p>Ficarei feliz em saber mais sobre seus projetos e como podemos trabalhar juntos para torná-los realidade</p>
-          <p>igor_contatos@hotmail.com</p>
-          <p>(31) 99802-3935</p>
-          <ul style={{listStyle: "none", display: "flex", alignItems: "center", justifyContent: "space-evenly"}}>
-            <li>Instagram</li>
-            <li>Linkedin</li>
-            <li>Github</li>
-            <li>Whatsapp</li>
-          </ul>
-        </div>
-      </div>
-      
-      {/* 
-      <FooterContainer id="fale-comigo">
-      <FooterSocialContainer>
-        <FooterSocialWrapper>
-          <FooterTitle>REDES SOCIAIS</FooterTitle>
-          <FooterSocialLinkWrapper href="https://www.linkedin.com/in/igor-perdigao-silva/" target="_blank" rel="noopener noreferrer">
-            <LinkedinIcon />
-            <FooterLink>Linkedin</FooterLink>
-          </FooterSocialLinkWrapper>
-          <FooterSocialLinkWrapper href="https://github.com/Hashtag-igor" target='_blank' rel="noopener noreferrer">
-            <GithubIcon />
-            <FooterLink>Github</FooterLink>
-          </FooterSocialLinkWrapper>
-        </FooterSocialWrapper>
-
-        <FooterInfoContainer>
-            <FooterTitle>CONTATO</FooterTitle>
-            <FooterInfoWrapper>
-              <PhoneIcon />
-              <FooterLink>whatsapp (31) 99802-3935</FooterLink>
-            </FooterInfoWrapper>
-            <FooterInfoWrapper>
-              <MailIcoon />
-              <FooterLink>igor_contatos@hotmail.com</FooterLink>
-            </FooterInfoWrapper>
-        </FooterInfoContainer>
-      </FooterSocialContainer> 
-        </FooterContainer >
-      */
-      
-      }
+    <FooterContainer id='fale-comigo'>
+      <FooterProfileContainer>
+        <FooterProfileWrapper>
+          <FooterTitle>Aberto à Conversa!</FooterTitle>
+          <FooterDescription>Estou entusiasmado para aprender mais sobre seus projetos. Juntos, podemos explorar maneiras de transformar suas ideias em realidade. Estou ansioso para discutir como podemos colaborar e alcançar objetivos comuns.</FooterDescription>
+          <FooterName>igor_contatos@hotmail.com</FooterName>
+          <FooterNumber>(31) 99802-3935</FooterNumber>
+          <FooterLinksContainer>
+            <FooterLinksWrapper to="https://www.instagram.com/igorperdigao_s/" target="_blank">
+              <InstagramIcon />
+            </FooterLinksWrapper>
+            <FooterLinksWrapper to="https://www.linkedin.com/in/igor-perdigao-silva/" target="_blank">
+              <LinkedinIcon />
+            </FooterLinksWrapper>
+            <FooterLinksWrapper to="https://github.com/Hashtag-igor" target="_blank">
+              <GithubIcon />
+            </FooterLinksWrapper>
+            <FooterLinksWrapper to="https://wa.me/5531998023935" target="_blank">
+              <WhatsappIcon />
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+        </FooterProfileWrapper>
+      </FooterProfileContainer>
 
       <RightsReservedContainer>
-        <FooterName>© 2023 Igor Perdigão. Todos os direitos reservados.</FooterName>
+        <FooterReserved>© 2023 Igor Perdigão. Todos os direitos reservados.</FooterReserved>
       </RightsReservedContainer>
-    </div>
+    </FooterContainer>
   )
 }
