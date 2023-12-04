39246d9a598af84b290e022c04bb5ae79d29fcd6
Igor Perdigão <77364859+Hashtag-igor@users.noreply.github.com>
2023-12-04 13:22:47 -0300

    Modifica informações do projeto FinancialSystem

--- src/pages/FinancialSystem.jsx
+++ src/pages/FinancialSystem.jsx
@@ -6,8 +6,8 @@ export default function FinancialSystem() {
   const FinancialSystemData = {
     title: "Financial System",
     img: financialSystem,
-    technologiesUsed: "Tecnologias Utilizadas: React, Node.js e Mongodb",
-    description: "Projeto atualmente em desenvolvimento..",
+    technologiesUsed: "Tecnologias Utilizadas: React, Typescript , Node.js e Mongodb",
+    description: "Estou trabalhando no desenvolvimento de um sistema financeiro pessoal que permite aos usuários criar contas, fazer login e acessar facilmente suas finanças mensais. O projeto inclui autenticação segura, visão abrangente das finanças atuais, arquivamento automático de meses anteriores e detalhes precisos sobre transações. Utilizo tecnologias avançadas, como Node.js, React, MongoDB e TypeScript para garantir uma implementação segura e eficaz. O objetivo é proporcionar aos usuários uma ferramenta intuitiva para gerenciar suas finanças pessoais. Vale lembrar que o projeto atualmente está em desenvolvimento..",
     firstlink: "https://github.com/Hashtag-igor/Financial-System",
     link: "Em breve estará disponivel o link do projeto com hospedagem Amazon Web Services (AWS)..",
     carouselsImages: [
